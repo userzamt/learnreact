b34838419fce8712bd657540e1a8bc6075001e4c
userzamt <zamt.it@yandex.ru>
2022-12-27 22:14:20 +0300

    динамические классы

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -3,12 +3,30 @@ import React from "react";
 import "./Car.css";
 
 export default (props) =>{
-    console.log("Car");
+    // console.log("Car");
+    const carClasses = ["input"];
+
+    if(props.title !== ""){
+        carClasses.push("green");
+    } else {
+        carClasses.push("red");
+        carClasses.push("bold");
+    }
+
+    if(props.title.length > 3) {
+        carClasses.push("bold");
+    }
+
     return(
         <article className="Car">
             <h2>{props.title}</h2>
             <p>{props.description}</p>
-            <input type="text"  onChange={props.onChangeName} value={props.title}/>
+            <input 
+                type="text"  
+                onChange={props.onChangeName} 
+                value={props.title}
+                className={carClasses.join(" ")}
+            />
             <button onClick={props.onDelete}>Delete</button>
             <small>{props.referens}</small>
         </article>
